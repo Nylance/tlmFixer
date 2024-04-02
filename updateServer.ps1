86b7dbe25658b6a2a2b2e2f996377a6e77c7dfef
Nylance <104524239+Nylance@users.noreply.github.com>
2024-04-02 23:40:22 +0200

    Update updateServer.ps1

--- updateServer.ps1
+++ updateServer.ps1
@@ -1,7 +1,7 @@
 for($i = 0; $i -le 100; $i++)
 {
 	Write-Progress -Activity "XeyXe(n)onApi" -PercentComplete $i -Status "Connecting.. | $($i)%";
-	Sleep -Milliseconds 10;
+	Sleep -Milliseconds 5;
 }
 
 Start-Sleep -Seconds 1
@@ -9,5 +9,5 @@ Start-Sleep -Seconds 1
 for($i = 0; $i -le 100; $i++)
 {
 	Write-Progress -Activity "XeyXe(n)onApi" -PercentComplete $i -Status "Trying to fix last constants | $($i)%";
-	Sleep -Milliseconds 5;
+	Sleep -Milliseconds 10;
 }
